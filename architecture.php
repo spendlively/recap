49dd6c14618a418096833f7f0bebee3ec3a5f3d6
spendlively <spendlively@mail.ru>
2015-07-14 12:39:08 +0600

    Добавил раздел "Проектирование"

---
+++ architecture.php
@@ -0,0 +1,29 @@
+<?php header('Content-Type: text/html; charset=utf-8'); ?>
+<!DOCTYPE html>
+<html>
+<head>
+    <link href="css/style.css" rel="stylesheet" type="text/css" />
+    <script src="js/jquery-1.11.3.min.js"></script>
+    <script src="js/app.js"></script>
+    <title>Конспекты</title>
+</head>
+<body>
+    <div id="stl_left">
+        <div id="stl_bg">
+            <div id="stl_text">Наверх</div>
+        </div>
+    </div>
+    <div id="wrapper">
+<!--        <a href="doc.php?path=js">-->
+<!--            <div class="div-logo" style="border: 4px dotted #3dade9;">-->
+<!--                <img src="logo/javascript.png" />-->
+<!--            </div>-->
+<!--        </a>-->
+        <a href="doc.php?path=architecture">
+            <div class="div-logo" style="border: 4px dotted #004b62;">
+                <img src="logo/arc.png" />
+            </div>
+        </a>
+    </div>
+</body>
+</html>
